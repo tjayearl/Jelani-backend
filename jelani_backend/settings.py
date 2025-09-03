021192b53e0e7582f5d87367f6e0e0cb243d6e5d
tjayearl <iamtjayearl@gmail.com>
2025-09-03 16:31:22 +0300

    modified

--- jelani_backend/settings.py
+++ jelani_backend/settings.py
@@ -116,8 +116,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = "jelaniafrika@gmail.com"
-EMAIL_HOST_PASSWORD = "jzbk eppz ictq hcis"  # WARNING: Hardcoded password is a security risk!
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 # CORS Settings
