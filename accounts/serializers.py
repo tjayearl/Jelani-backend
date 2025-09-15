28dfadee271baf84fcc412bc9809294e6af4f377
tjayearl <iamtjayearl@gmail.com>
2025-09-15 07:33:06 +0300

    modified

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from django.contrib.auth import authenticate, get_user_model
 
+from .models import Claim, Payment
 User = get_user_model()
 
 class CustomLoginSerializer(serializers.Serializer):
@@ -27,3 +28,15 @@ class CustomLoginSerializer(serializers.Serializer):
 
         data["user"] = user
         return data
+
+class ClaimSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Claim
+        fields = "__all__"
+        read_only_fields = ('user', 'status') # User is set from request, status is managed internally
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
+        fields = "__all__"
+        read_only_fields = ('user', 'status', 'reference') # These are set by the server
